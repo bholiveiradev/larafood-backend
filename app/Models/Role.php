8cabb9c006c1dc220c21f2c47243b3ba2e426882
Bruno Oliveira <83931779+bholiveiradev@users.noreply.github.com>
2023-05-28 05:55:28 -0300

    Fix code style

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -24,15 +24,15 @@ class Role extends Model
     public function available($userId, $text = '')
     {
         $roles = $this->whereNotIn('id', function ($query) use ($userId) {
-                        $query->select('role_user.role_id')
-                              ->from('role_user')
-                              ->whereRaw("role_user.user_id={$userId}");
-                    });
+            $query->select('role_user.role_id')
+                ->from('role_user')
+                ->whereRaw("role_user.user_id={$userId}");
+        });
 
         if(!empty($text)) {
             $roles->where('name', 'LIKE', "%{$text}%")
-                     ->orWhere('description', 'LIKE', "%{$text}%")
-                     ->latest();
+                ->orWhere('description', 'LIKE', "%{$text}%")
+                ->latest();
         }
 
         return $roles;
